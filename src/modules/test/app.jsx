4891bc9ec4deedbf21d1d59d74b37aa56941aa9a
lianruhe <lianruhe2013@gmail.com>
2017-07-27 17:14:50 +0800

    [M] update test

--- src/modules/test/app.jsx
+++ src/modules/test/app.jsx
@@ -1,11 +1,84 @@
 import React from 'react'
 // import Base from 'components/base'
+import { Steps, Button, message, Icon, Row, Col } from 'antd'
+import FormStep1 from './step1-form'
+import 'styles/app/test.css'
+
+const Step = Steps.Step
+
+const steps = [{
+  title: '文件上传',
+  icon: 'upload',
+  content: 'First-content'
+}, {
+  title: '文件校验',
+  icon: 'exception',
+  content: 'Second-content'
+}, {
+  title: '完成',
+  icon: 'check-circle-o',
+  content: 'Last-content'
+}]
 
 export default class Test extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      formStep1: {},
+      current: 0
+    }
+  }
+
+  next () {
+    const { current } = this.state
+    const component = `formStep${current + 1}`
+    this.refs[component].stepComponent.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log(values)
+        this.setState({
+          [component]: values,
+          current: current + 1
+        })
+      }
+    })
+  }
+
+  prev () {
+    const current = this.state.current - 1
+    this.setState({ current })
+  }
+
   render () {
+    const { current, formStep1 } = this.state
     return (
-      <div id="ui-home">
-        This is a test page!
+      <div className="ui-test">
+        <div className="ui-wrapper" id="steps-wrapper">
+          <Steps className="steps" current={current} status="process">
+            {steps.map(item => <Step key={item.title} title={item.title} icon={<Icon type={item.icon} />} />)}
+          </Steps>
+        </div>
+        <div className="ui-wrapper">
+          { current === 0 && <FormStep1 ref="formStep1" formData={formStep1} />}
+          { current !== 0 && <div className="content">{steps[current].content}</div>}
+          <Row className="action">
+            <Col span={8} offset={16}>
+              {
+                this.state.current > 0 &&
+                <Button style={{ marginRight: 10 }} onClick={() => this.prev()}>
+                  上一步
+                </Button>
+              }
+              {
+                this.state.current < steps.length - 1 &&
+                <Button type="primary" onClick={() => this.next()}>下一步</Button>
+              }
+              {
+                this.state.current === steps.length - 1 &&
+                <Button type="primary" onClick={() => message.success('Processing complete!')}>完成</Button>
+              }
+            </Col>
+          </Row>
+        </div>
       </div>
     )
   }
---
+++ src/modules/test/step1-form.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Form from 'components/form'
+import { Input, Upload, Button, Icon } from 'antd'
+
+const { TextArea } = Input
+
+export default class Step1 extends React.Component {
+  static propTypes = {
+    formData: PropTypes.object
+  }
+
+  constructor (props) {
+    super(props)
+
+    const { name, detail, file } = props.formData || {}
+    this.state = {
+      name,
+      detail,
+      file,
+      uploading: false
+    }
+  }
+
+  saveFormValues (file) {
+    const values = this.stepComponent.props.form.getFieldsValue()
+    const { name, detail } = values
+    this.setState({
+      name,
+      detail,
+      file
+    })
+  }
+
+  render () {
+    const { name, detail, file } = this.state
+    const uploadProps = {
+      action: '//jsonplaceholder.typicode.com/posts/',
+      onRemove: (file) => {
+        this.saveFormValues()
+      },
+      beforeUpload: (file) => {
+        console.log(file)
+        this.saveFormValues(file)
+        return false
+      },
+      fileList: file ? [file] : []
+    }
+
+    const items = [{
+      label: '任务名称',
+      id: 'name',
+      options: {
+        rules: [{ required: true, message: 'Please select your country!' }],
+        initialValue: name
+      },
+      col: {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 10 }
+      },
+      element: <Input placeholder="Username" />
+    }, {
+      label: '任务描述',
+      id: 'detail',
+      options: {
+        rules: [{ required: true, message: 'Please input your Password!' }],
+        initialValue: detail
+      },
+      col: {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 10 }
+      },
+      element: <TextArea rows={2} placeholder="Password" />
+    }, {
+      label: '文件上传',
+      id: 'file',
+      options: {
+        rules: [{ required: true, message: 'Please input your Password!' }],
+        initialValue: file
+      },
+      col: {
+        labelCol: { span: 8 },
+        wrapperCol: { span: 10 }
+      },
+      element: <Upload {...uploadProps}>
+        <Button>
+          <Icon type="upload" /> 选择上传文件
+        </Button>
+      </Upload>
+    }]
+    return (
+      <Form className="ui-step1-form"
+        wrappedComponentRef={inst => { this.stepComponent = inst }}
+        layout="horizontal"
+        items={items} />
+    )
+  }
+}
